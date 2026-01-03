9b0ccee13d096c2fae6188114c13fcdda26c2833
23120383 <patricktran287@gmail.com>
2026-01-03 17:56:31 +0700

    refactor(Onboarding): Simplify splash screen logic
    - Refactors the splash screen to act as a simple navigator.
    - It now checks the user's login state and immediately redirects to the appropriate screen (Welcome or Main) without displaying a layout, providing a faster startup experience.

--- app/src/main/java/com/example/Smart_Chat/activities/login/splashScreenActivity.kt
+++ app/src/main/java/com/example/Smart_Chat/activities/login/splashScreenActivity.kt
@@ -7,9 +7,6 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.Smart_Chat.R
 import com.example.Smart_Chat.activities.MainActivity
-import com.example.Smart_Chat.activities.user_chat.ChatActivity
-import com.example.Smart_Chat.models.userModel
-import com.example.Smart_Chat.utils.others.androidUtils
 import com.example.Smart_Chat.utils.firebase.FirebaseAuthentication
 
 class splashScreenActivity : AppCompatActivity() {
@@ -17,34 +14,15 @@ class splashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        if (FirebaseAuthentication.isLoggedIn && intent.extras != null) {
-            val userID = intent.extras?.getString("userID")
-
-            userID?.let {
-                FirebaseAuthentication.allUsersCollection().document(it).get()
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            val model = task.result.toObject(userModel::class.java)
-
-                            val mainIntent = Intent(this, MainActivity::class.java).apply {
-                                flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
-                            }
-                            startActivity(mainIntent)
-
-                            val chatIntent = Intent(this, ChatActivity::class.java).apply {
-                                flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                            }
-                            androidUtils.passUserModelAsIntent(chatIntent, model)
-                            startActivity(chatIntent)
-                        }
-                    }
-            }
-        } else {
-            Handler(Looper.getMainLooper()).postDelayed({
-                val intent = Intent(this@splashScreenActivity, WelcomeActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (FirebaseAuthentication.isLoggedIn) {
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
-                finish()
-            }, 2000)
-        }
+            } else {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+            }
+            finish()
+        }, 2000)
     }
 }
