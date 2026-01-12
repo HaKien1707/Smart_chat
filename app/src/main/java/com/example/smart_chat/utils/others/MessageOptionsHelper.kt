058dd244bf7ef87e192408120507940da18544a9
23120383 <patricktran287@gmail.com>
2026-01-13 03:26:07 +0700

    fix: fix reply and forward layout

--- app/src/main/java/com/example/smart_chat/utils/others/MessageOptionsHelper.kt
+++ app/src/main/java/com/example/smart_chat/utils/others/MessageOptionsHelper.kt
@@ -1,6 +1,7 @@
 package com.example.smart_chat.utils.others
 
 import android.content.Context
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
@@ -28,33 +29,54 @@ object MessageOptionsHelper {
             true
         )
 
+        val optionReplyContainer = popupView.findViewById<View>(R.id.option_reply_container)
+        val optionForwardContainer = popupView.findViewById<View>(R.id.option_forward_container)
+        val optionDeleteContainer = popupView.findViewById<View>(R.id.option_delete_container)
+
         val optionReply = popupView.findViewById<TextView>(R.id.option_reply)
         val optionForward = popupView.findViewById<TextView>(R.id.option_forward)
         val optionDelete = popupView.findViewById<TextView>(R.id.option_delete)
 
+        val dividerForward = popupView.findViewById<View>(R.id.divider_forward)
+
         // Reply option
-        optionReply.setOnClickListener {
+        val replyClickListener = View.OnClickListener {
             popupWindow.dismiss()
             onReply(messageData)
         }
+        optionReplyContainer.setOnClickListener(replyClickListener)
+        optionReply.setOnClickListener(replyClickListener)
 
         // Forward option
-        optionForward.setOnClickListener {
+        val forwardClickListener = View.OnClickListener {
             popupWindow.dismiss()
             onForward()
         }
+        optionForwardContainer.setOnClickListener(forwardClickListener)
+        optionForward.setOnClickListener(forwardClickListener)
 
         // Delete option (only show if user can delete)
         if (canDelete) {
-            optionDelete.visibility = View.VISIBLE
-            optionDelete.setOnClickListener {
+            optionDeleteContainer.visibility = View.VISIBLE
+            dividerForward.visibility = View.VISIBLE
+
+            val deleteClickListener = View.OnClickListener {
                 popupWindow.dismiss()
                 onDelete()
             }
+
+            optionDeleteContainer.setOnClickListener(deleteClickListener)
+            optionDelete.setOnClickListener(deleteClickListener)
         } else {
-            optionDelete.visibility = View.GONE
+            optionDeleteContainer.visibility = View.GONE
+            dividerForward.visibility = View.GONE
         }
 
-        popupWindow.showAsDropDown(view, 0, -view.height)
+        val gapPx = dpToPx(6)
+        popupWindow.showAsDropDown(view, 0, -view.height - gapPx)
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        return (dp * Resources.getSystem().displayMetrics.density).toInt()
     }
 }
